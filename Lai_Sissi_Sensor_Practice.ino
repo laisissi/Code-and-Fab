cefd01bfc0d67957863581cde1440c313a295654
laisissi <71714778+laisissi@users.noreply.github.com>
2025-11-05 13:02:39 -0600

    Add files via upload

---
+++ Lai_Sissi_Sensor_Practice.ino
@@ -0,0 +1,24 @@
+// Lai_Sissi_Sensor_Practice
+
+int sensorPin = A0; // photoresistor connected to A0
+int ledPin = 13;    // LED on pin 13
+int sensorValue = 0; 
+
+void setup()
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin); // reads value 0-1023
+  Serial.println(sensorValue);
+
+  // Decide threshold for LED
+  if(sensorValue < 100){ // dark environment
+    digitalWrite(ledPin, HIGH); // turn on LED
+  } else { // bright environment
+    digitalWrite(ledPin, LOW); // turn off LED
+  }
+
+  delay(100); // small delay
+}
